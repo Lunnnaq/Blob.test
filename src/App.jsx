bec14f7c249184257daefb23da62555230bb830b
CodeSandbox <support@codesandbox.io>
2023-03-25 20:41:29 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/88IgjAJZGFQfsSnC/scene.splinecode" />
+   
+  );
+  
+
+
+
+
+}
